191b823462dd8bdac2ff31120a2c27803fdd1663
Dmitry Kurochkin <kudima03@gmail.com>
2025-10-07 14:58:21 +0300

    Tests improved.

--- src/Tests/Pure.RelationalSchema.Self.Storage.Projection.Tests/SchemaProjectionTests.cs
+++ src/Tests/Pure.RelationalSchema.Self.Storage.Projection.Tests/SchemaProjectionTests.cs
@@ -5,11 +5,19 @@ namespace Pure.RelationalSchema.Self.Storage.Projection.Tests;
 public sealed record SchemaProjectionTests
 {
     [Fact]
-    public void Enumerates()
+    public void CorrectGroupCount()
     {
         Assert.Equal(
             10,
             new SchemaProjection(new RandomSchema()).Select(x => x.ToArray()).Count()
         );
+    }
+
+    [Fact]
+    public void EnumerateCells()
+    {
+        int count = new SchemaProjection(new RandomSchema())
+            .SelectMany(x => x.SelectMany(c => c.Cells.Values.Select(v => v.Value)).ToArray()).Count();
+        Assert.NotEqual(0, count);
     }
 }
