2c637c315a3ae9dfafe5246075d136eedd18589d
Dmitry Kurochkin <kudima03@gmail.com>
2025-10-30 15:49:10 +0300

    Added ProduceCorrectColumnsInForeignKeysRows test

--- src/Tests/Pure.RelationalSchema.Self.Storage.Projection.Tests/SchemaProjectionTests.cs
+++ src/Tests/Pure.RelationalSchema.Self.Storage.Projection.Tests/SchemaProjectionTests.cs
@@ -459,6 +459,28 @@ public sealed record SchemaProjectionTests
     }
 
     [Fact]
+    public void ProduceCorrectColumnsInForeignKeysRows()
+    {
+        IGrouping<ITable, IRow> projection = new SchemaProjection(
+            new RelationalSchemaSchema()
+        ).Single(x =>
+            new TableHash(x.Key).SequenceEqual(new TableHash(new ForeignKeysTable()))
+        );
+
+        Assert.True(
+            projection.All(row =>
+                new DeterminedHash(
+                    row.Cells.Keys.Select(column => new ColumnHash(column))
+                ).SequenceEqual(
+                    new DeterminedHash(
+                        projection.Key.Columns.Select(column => new ColumnHash(column))
+                    )
+                )
+            )
+        );
+    }
+
+    [Fact]
     public void CorrectGroupCount()
     {
         Assert.Equal(
