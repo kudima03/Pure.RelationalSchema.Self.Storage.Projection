14e987b9aada61c4f41ccf9bd418d1f0ca8157e2
Dmitry Kurochkin <kudima03@gmail.com>
2025-10-30 11:58:36 +0300

    Added ProduceCorrectGroupingTables test

--- src/Tests/Pure.RelationalSchema.Self.Storage.Projection.Tests/SchemaProjectionTests.cs
+++ src/Tests/Pure.RelationalSchema.Self.Storage.Projection.Tests/SchemaProjectionTests.cs
@@ -1,10 +1,28 @@
 using System.Data;
+using Pure.HashCodes;
+using Pure.RelationalSchema.Abstractions.Schema;
+using Pure.RelationalSchema.HashCodes;
 using Pure.RelationalSchema.Self.Schema;
 
 namespace Pure.RelationalSchema.Self.Storage.Projection.Tests;
 
 public sealed record SchemaProjectionTests
 {
+    [Fact]
+    public void ProduceCorrectGroupingTables()
+    {
+        ISchema schema = new RelationalSchemaSchema();
+        Assert.True(
+            new DeterminedHash(schema.Tables.Select(x => new TableHash(x))).SequenceEqual(
+                new DeterminedHash(
+                    new SchemaProjection(schema)
+                        .Select(x => x.Key)
+                        .Select(x => new TableHash(x))
+                )
+            )
+        );
+    }
+
     [Fact]
     public void CorrectGroupCount()
     {
@@ -20,7 +38,7 @@ public sealed record SchemaProjectionTests
     public void CorrectCellsCount()
     {
         Assert.Equal(
-            305,
+            339,
             new SchemaProjection(new RelationalSchemaSchema())
                 .SelectMany(x =>
                     x.SelectMany(c => c.Cells.Values.Select(v => v.Value)).ToArray()
