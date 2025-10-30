30be3736bb3237054d9d1a71d8971d5d76df5777
Dmitry Kurochkin <kudima03@gmail.com>
2025-10-30 15:02:51 +0300

    Added ProduceCorrectColumnsInTablesToColumnsRows test

--- src/Tests/Pure.RelationalSchema.Self.Storage.Projection.Tests/SchemaProjectionTests.cs
+++ src/Tests/Pure.RelationalSchema.Self.Storage.Projection.Tests/SchemaProjectionTests.cs
@@ -305,6 +305,27 @@ public sealed record SchemaProjectionTests
     }
 
     [Fact]
+    public void ProduceCorrectColumnsInTablesToColumnsRows()
+    {
+        ISchema schema = new RelationalSchemaSchema();
+        IGrouping<ITable, IRow> projection = new SchemaProjection(schema).Single(x =>
+            new TableHash(x.Key).SequenceEqual(new TableHash(new TablesToColumnsTable()))
+        );
+
+        Assert.True(
+            projection.All(row =>
+                new DeterminedHash(
+                    row.Cells.Keys.Select(column => new ColumnHash(column))
+                ).SequenceEqual(
+                    new DeterminedHash(
+                        projection.Key.Columns.Select(column => new ColumnHash(column))
+                    )
+                )
+            )
+        );
+    }
+
+    [Fact]
     public void CorrectGroupCount()
     {
         Assert.Equal(
