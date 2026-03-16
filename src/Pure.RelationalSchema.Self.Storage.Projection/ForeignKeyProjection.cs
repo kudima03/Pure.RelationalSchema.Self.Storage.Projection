fe1bed51db2474853561c00edd11fd5adbd96f7f
Dmitry Kurochkin <kudima03@gmail.com>
2026-03-16 10:57:51 +0300

    ForeignKeyProjection reworked

--- src/Pure.RelationalSchema.Self.Storage.Projection/ForeignKeyProjection.cs
+++ src/Pure.RelationalSchema.Self.Storage.Projection/ForeignKeyProjection.cs
@@ -1,9 +1,6 @@
 using Pure.Collections.Generic;
-using Pure.HashCodes.Abstractions;
-using Pure.Primitives.String.Operations;
+using Pure.Primitives.Abstractions.Guid;
 using Pure.RelationalSchema.Abstractions.Column;
-using Pure.RelationalSchema.Abstractions.ForeignKey;
-using Pure.RelationalSchema.Abstractions.Table;
 using Pure.RelationalSchema.HashCodes;
 using Pure.RelationalSchema.Self.Schema.Columns;
 using Pure.RelationalSchema.Self.Schema.Tables;
@@ -16,56 +13,53 @@ namespace Pure.RelationalSchema.Self.Storage.Projection;
 
 internal sealed record ForeignKeyProjection : IRow
 {
-    private readonly IForeignKey _entity;
-
-    private readonly IEnumerable<IColumn> _columns;
-
-    private readonly IReadOnlyDictionary<ITable, IDeterminedHash> _tablesCache;
-
-    private readonly IReadOnlyDictionary<IColumn, IDeterminedHash> _columnsCache;
+    public ForeignKeyProjection(
+        IGuid referenceToReferencingTable,
+        IGuid referenceToReferencedTable
+    )
+        : this(
+            referenceToReferencingTable,
+            referenceToReferencedTable,
+            new ForeignKeysTable().Columns
+        )
+    { }
 
     public ForeignKeyProjection(
-        IForeignKey entity,
-        IReadOnlyDictionary<IColumn, IDeterminedHash> columnsCache,
-        IReadOnlyDictionary<ITable, IDeterminedHash> tablesCache
+        IGuid referenceToReferencingTable,
+        IGuid referenceToReferencedTable,
+        IEnumerable<IColumn> columns
     )
-        : this(entity, new ForeignKeysTable().Columns, columnsCache, tablesCache) { }
+        : this(
+            new Dictionary<IColumn, IColumn, ICell>(
+                columns,
+                column => column,
+                column => new CellSwitch<IColumn>(
+                    column,
+                    [
+                        new KeyValuePair<IColumn, ICell>(
+                            new UuidColumn(),
+                            new Cell(new String(new Ulid()))
+                        ),
+                        new KeyValuePair<IColumn, ICell>(
+                            new ReferencingTableColumn(),
+                            new Cell(new String(referenceToReferencingTable))
+                        ),
+                        new KeyValuePair<IColumn, ICell>(
+                            new ReferencedTableColumn(),
+                            new Cell(new String(referenceToReferencedTable))
+                        ),
+                    ],
+                    column => new ColumnHash(column)
+                ),
+                column => new ColumnHash(column)
+            )
+        )
+    { }
 
-    public ForeignKeyProjection(
-        IForeignKey entity,
-        IEnumerable<IColumn> columns,
-        IReadOnlyDictionary<IColumn, IDeterminedHash> columnsCache,
-        IReadOnlyDictionary<ITable, IDeterminedHash> tablesCache
-    )
+    private ForeignKeyProjection(IReadOnlyDictionary<IColumn, ICell> cells)
     {
-        _entity = entity;
-        _columns = columns;
-        _tablesCache = tablesCache;
-        _columnsCache = columnsCache;
+        Cells = cells;
     }
 
-    public IReadOnlyDictionary<IColumn, ICell> Cells =>
-        new Dictionary<IColumn, IColumn, ICell>(
-            _columns,
-            column => column,
-            column => new CellSwitch<IColumn>(
-                column,
-                [
-                    new KeyValuePair<IColumn, ICell>(
-                        new UuidColumn(),
-                        new Cell(new String(new Ulid()))
-                    ),
-                    new KeyValuePair<IColumn, ICell>(
-                        new ReferencingTableColumn(),
-                        new Cell(new HexString(_tablesCache[_entity.ReferencingTable]))
-                    ),
-                    new KeyValuePair<IColumn, ICell>(
-                        new ReferencedTableColumn(),
-                        new Cell(new HexString(_tablesCache[_entity.ReferencedTable]))
-                    ),
-                ],
-                column => new ColumnHash(column)
-            ),
-            column => new ColumnHash(column)
-        );
+    public IReadOnlyDictionary<IColumn, ICell> Cells { get; }
 }
